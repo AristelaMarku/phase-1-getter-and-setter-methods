15f87979a90570b6123eb1364869bf4f39c2847f
Aristela Marku <aristela.marku@gmail.com>
2021-10-07 21:50:46 +0200

    commit tree 545eca500091d774e7fa753739df14c7f817f7bf

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+
+class Circle{
+    constructor(radius){
+        this.radius=radius;
+    }
+    get diameter(){
+        return 2*this.radius
+    }
+
+    get circumference(){
+        return 2* Math.PI * this.radius
+
+    }
+
+    get area (){
+        return Math.Pi * this.radius* this.radius
+    }
+
+
+    set radius(radius) {
+        this._radius = radius;
+      }
+
+}
+
+let circle=new Circle(6)
+circle.radius
+circle.radius=3
+circle.circumference=6
+circle.area=6
